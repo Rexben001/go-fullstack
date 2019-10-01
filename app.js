ef3610d0a10dcaddf7bb5f59e1f7211fbff89a84
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-10-01 17:37:45 +0100

    add cross origin refrences to Rest Api

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-const express = require("express");// imports express
+const express = require('express');// imports express
 
 const app = express();
 
-//middleware
-app.use((req, res)=>{
-    res.json({ message :'this is an express sserver'});
-});
+//middlewares
+
+//setting cors
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    next();
+
+}); //allows access to api in any platform
+
+app.use('/api/stuff', (req, res)=>{
+    res.status(200).json([
+        {
+            _id: "thingid",
+            title: "first thing",
+            description: "description of first thing",
+            imageUrl: "",
+            price: 2900,
+            userId: "Userid",
+          },
+          {
+            _id: "thing2id",
+            title: "secondthing",
+            description: "descripton of second thing",
+            imageUrl: "",
+            price: 4900,
+            userId: "Userid"
+          }
+        ]
+    )
+})
+
+
 
 module.exports = app; //allows app to be imported to other files
