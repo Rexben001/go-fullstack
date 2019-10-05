a81071e70286782e4fc49d915b14629968d1611b
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-10-05 01:25:36 +0100

    create model for thing schema

--- models/thing.js
+++ models/thing.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.modelSchema({
+const thingSchema = mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     imageUrl: {type: String, required: true},
